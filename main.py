6028aeda683fcaac1640b1e134d8293d6f849b3b
Ivan Dorofeev <axxel123@rambler.ru>
2023-04-07 09:06:27 +0300

    add steps

--- main.py
+++ main.py
@@ -2,68 +2,131 @@ import json
 import logging
 import os
 import random
-
+import redis
 import telegram
+from functools import partial
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler, \
+    RegexHandler
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+WAIT_START_QUIZ, WAIT_ANSWER = range(2)
+
+custom_keyboard = [['Новый вопрос', 'Сдаться', 'Мой счёт']]
+reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(fr'Привет, {user.mention_markdown_v2()}\!',
-                                     reply_markup=ForceReply(selective=True))
+                                     reply_markup=reply_markup)
+    return WAIT_START_QUIZ
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
     custom_keyboard = [['Новый вопрос', 'Сдаться', 'Мой счёт']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_text('Help!', reply_markup=reply_markup)
+    return WAIT_START_QUIZ
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    custom_keyboard = [['Новый вопрос', 'Сдаться', 'Мой счёт']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+def handle_new_question_request(update: Update, context: CallbackContext, conn, json_quiz):
+    """Задаём рандомный вопрос"""
 
-    if update.message.text == 'Новый вопрос':
-        """Отправляем рандомный вопрос викторины"""
-        with open('questions_and_answers.json', 'r') as quiz_file:
-            json_quiz = json.load(quiz_file)
-        random_question_number = random.choice(list(json_quiz.keys()))
-        random_question = json_quiz[random_question_number]['question']
+    user_id = update.message.from_user.id
+    user_msg = update.message.text
+    print(user_id, ' = ', user_msg)
 
-        update.message.reply_text(random_question, reply_markup=reply_markup)
+    random_question_number = random.choice(list(json_quiz.keys()))
+    random_question = json_quiz[random_question_number]['question']
+
+    # Записываем пользователя и вопрос в базу
+    conn.set(user_id, random_question_number)
+    update.message.reply_text(random_question, reply_markup=reply_markup)
+
+    return WAIT_ANSWER
+
+
+def handle_solution_attempt(update: Update, context: CallbackContext, conn, json_quiz):
+    """Проверяем ответ на рандомный вопрос"""
+    user_id = update.message.from_user.id
+    user_msg = update.message.text
+
+    number_question_of_user = conn.get(user_id)
+    if number_question_of_user:
+        answer = json_quiz[number_question_of_user]['answer']
+
+        user_msg_first_text = user_msg.split('.')[0]
+        if user_msg_first_text in answer:
+            update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+                                      reply_markup=reply_markup)
+        else:
+            update.message.reply_text(f'Увы, Не верно.', reply_markup=reply_markup)
+            return WAIT_ANSWER
     else:
-        update.message.reply_text(update.message.text, reply_markup=reply_markup)
+        update.message.reply_text('Нажми «Новый вопрос»', reply_markup=reply_markup)
+        return WAIT_START_QUIZ
 
 
-def main(bot_token) -> None:
+def cancel_quiz(update: Update, context: CallbackContext, conn, json_quiz):
+    user_id = update.message.from_user.id
+
+    number_question_of_user = conn.get(user_id)
+    if number_question_of_user:
+        answer = json_quiz[number_question_of_user]['answer']
+        update.message.reply_text(f'Правильный ответ:\n{answer}', reply_markup=reply_markup)
+        conn.delete(user_id)  # Удаляем пользователя и вопрос из базы
+
+        handle_new_question_request(update=update, context=context, conn=conn, json_quiz=json_quiz)
+    else:
+        update.message.reply_text(f'Радо сдаваться, ты ещё не начал =). Нажми "Новый вопрос"')
+        return WAIT_START_QUIZ
+
+
+def bot(bot_token, conn, json_quiz) -> None:
     updater = Updater(bot_token)
 
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+        states={
+            WAIT_START_QUIZ: [
+                MessageHandler(Filters.regex('^(Новый вопрос)$'),
+                               partial(handle_new_question_request, conn=conn, json_quiz=json_quiz))],
 
+            WAIT_ANSWER: [
+                MessageHandler(Filters.regex('^(Сдаться)$'), partial(cancel_quiz, conn=conn, json_quiz=json_quiz)),
+                MessageHandler(Filters.text, partial(handle_solution_attempt, conn=conn, json_quiz=json_quiz))
+            ]
+        },
+
+        fallbacks=[MessageHandler(Filters.regex('^(Сдаться)$'), partial(cancel_quiz, conn=conn, json_quiz=json_quiz))]
+    )
+
+    dispatcher.add_handler(conv_handler)
     updater.start_polling()
 
     updater.idle()
 
 
-def redis():
-    #  redis-cli -u redis://<username>:<password>@redis-15696.c1.us-east1-2.gce.cloud.redislabs.com:15696
-    pass
+def main():
+    load_dotenv()
+    bot_token = os.environ['TG_BOT_TOKEN']
+    redis_host = os.environ['REDIS_USER_HOST']
+    redis_port = os.environ['REDIS_USER_PORT']
+    redis_password = os.environ['REDIS_USER_PASSWORD']
+    with open('questions_and_answers.json', 'r') as quiz_file:
+        json_quiz = json.load(quiz_file)
+
+    conn = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+    bot(bot_token, conn, json_quiz)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    bot_token = os.environ['TG_BOT_TOKEN']
-    redis_name = os.environ['REDIS_USER_NAME']
-    redis_password = os.environ['REDIS_USER_PASSWORD']
-    main(bot_token)
+    main()
